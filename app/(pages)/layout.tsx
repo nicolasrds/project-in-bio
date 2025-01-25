ea6333b3fb9a9383dab7790df829f989634ee1c1
Nicolas Rodrigues <nicolasrds.nr@gmail.com>
2025-01-25 12:47:27 -0300

    add: Interface

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Red_Hat_Display } from 'next/font/google'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const redHatDisplay = Red_Hat_Display({
+    subsets: ["latin"],
+    weight: ["400", "500", "700"]
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,9 +19,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${redHatDisplay.className} bg-background-primary text-content-body antialiased`}
       >
         {children}
       </body>
---
+++ app/(pages)/page.tsx
@@ -0,0 +1,18 @@
+import Hero from "@/app/components/landing-page/Hero";
+import Header from "@/app/components/Header";
+import VideoExplanation from "@/app/components/landing-page/VideoExplanation";
+import Pricing from "@/app/components/landing-page/Princing";
+import FAQ from "@/app/components/landing-page/FAQ";
+
+
+export default function Home() {
+  return (
+      <div className="max-w-7xl mx-auto">
+          <Header />
+          <Hero />
+          <VideoExplanation />
+          <Pricing />
+          <FAQ />
+      </div>
+  );
+}
